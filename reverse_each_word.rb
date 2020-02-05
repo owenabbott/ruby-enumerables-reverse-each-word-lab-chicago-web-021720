82c49d7d3ccce2a80ad38a6b09eeae130b459b57
Owen Abbott <ocabbott@gmail.com>
2020-02-05 22:43:01 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+def reverse_each_word(string)
+  array = []
+  for a in string.split("")
+  array.push(a)
+end
+  #pp array
+  reverse_array = []
+  array.each do |x|
+    reverse_array.unshift(x)
+  end
+  reversed_string=reverse_array.join("")
+#pp reversed_string
+new_array = []
+  for a in reversed_string.split(" ")
+  new_array.unshift(a)
+end
+this_is_getting_too_long=[]
+new_array.each do |space|
+    this_is_getting_too_long.push (" "+space)
+end
+
+answer=this_is_getting_too_long.join("")
+pp answer
+return answer
+end
